5ef92d3c73519ebd517058bd4f7c2fdf5f13e034
Jacob <jacobharden3@gmail.com>
2025-05-23 16:02:11 +0100

    rebuilt off template with config

--- pipeline.py
+++ pipeline.py
@@ -2,57 +2,101 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions    
 from apache_beam.io.gcp.pubsub import ReadFromPubSub
 from apache_beam.io.gcp.bigquery import WriteToBigQuery, BigQueryDisposition
+import apache_beam.transforms.window as window
 import json
 import logging
 import os
 import yaml
-import retrieveSchema
+from datetime import datetime, timezone
 
 
 
-class CustomOptions(PipelineOptions):
-    @classmethod
-    def _add_argparse_args(cls, parser):
-        parser.add_value_provider_argument('--project', required=True, help='GCP project ID')
-        parser.add_value_provider_argument('--subscriptionName', required=True, help='Pub/Sub subscription name')
-        parser.add_value_provider_argument('--outputTable', required=True, help='BigQuery output table in the format project.dataset.table')
+# Defines the BigQuery schema for the output table.
+SCHEMA = ",".join(
+    [
+        "Type: STRING",
+        "Symbol: INTEGER",
+        "Open: DECIMAL:",
+        "High: DECIMAL",
+        "Low: DECIMAL",
+        "Close: DECIMAL",
+        "Volume: INTEGER",
+        "Timestamp: STRING",
+        "NumTradeTickers: INTEGER",
+        "VolumeWeightedPrice: DECIMAL",
+    ]
+)
 
-def runPipeline(argv=None):
 
-    pipeOptions = PipelineOptions(argv,
-                                  runner = 'DataflowRunner',
-                                  streaming = True,
-                                  save_main_session = True)
-    CustomOptions = PipelineOptions.view_as(CustomOptions)
+def parse_json_message(message: str) -> dict[str, Any]:
+    """Parse the input json message and add 'score' & 'processing_time' keys."""
+    row = json.loads(message)
+    return {
+        "Type": row["Type"],
+        "Symbol": row["Symbol"],
+        "Open": row["Open"],
+        "High": row["High"],
+        "Low": row["Low"],
+        "Close": row["Close"],
+        "Volume": row["Volume"],
+        "Timestamp": print(datetime.fromtimestamp(row['Timestamp'], tz=timezone.utc).strftime("%Y-%m-%d %H:%M:%S")), #conversion from POSIX timestamp to UTC
+        "NumTradeTickers": row["NumTradeTickers"],
+        "VolumeWeightedPrice": row["VolumeWeightedPrice"],
 
-    #retrieve custom options from CLI args
-    projectID = CustomOptions.project.get()
-    inputSub = CustomOptions.subScriptionName.get()
-    outputTable = CustomOptions.outputTable.get()
-
-    #retrieve schema from BigQuery table
-    schemaDict = retrieveSchema(projectID, outputTable)
-
-    #simplify schema dict for BQ definition
-    schemaDict = {
-        key: {
-            'type': str(value['type'])
-        }
-        for key, value in schemaDict.items()
     }
 
 
-    #define pipeline
-    with beam.Pipeline(options=pipeOptions) as p:
-        # Read from Pub/Sub
-        (p
-         | 'ReadFromPubSub' >> ReadFromPubSub(subscription=inputSub)
-         | 'ParseJson' >> beam.Map(lambda x: json.loads(x))
-         | 'TransformData' >> beam.Map(lambda x: {key: x.get(key, value['default']) for key, value in schemaDict.items()})
-         | 'WriteToBigQuery' >> WriteToBigQuery(
-             outputTable,
-             schema=schemaDict,
-             write_disposition=BigQueryDisposition.WRITE_APPEND,
-             create_disposition=BigQueryDisposition.CREATE_IF_NEEDED
-         )
+def run(
+    input_subscription: str,
+    output_table: str,
+    window_interval_sec: int = 60,
+    beam_args: list[str] = None,
+) -> None:
+    """Build and run the pipeline."""
+    options = PipelineOptions(beam_args, save_main_session=True, streaming=True)
+
+    with beam.Pipeline(options=options) as pipeline:
+        messages = (
+            pipeline
+            | "Read from Pub/Sub"
+            >> beam.io.ReadFromPubSub(
+                subscription=input_subscription
+            ).with_output_types(bytes)
+            | "UTF-8 bytes to string" >> beam.Map(lambda msg: msg.decode("utf-8"))
+            | "Parse JSON messages" >> beam.Map(parse_json_message)
+            | "Fixed-size windows"
+            >> beam.WindowInto(window.FixedWindows(window_interval_sec, 0))
         )
+
+        # Output the results into BigQuery table.
+        _ = messages | "Write to Big Query" >> beam.io.WriteToBigQuery(
+            output_table, schema=SCHEMA
+        )
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    config = yaml.safe_load(open("config.yaml")) #open configuration file
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        f"{config["bqProjectId"]}.{config["bqDatasetId"]}.{config["bqTableId"]}", #BQ output table
+    )
+    parser.add_argument(
+        f"projects/{config["bqProjectId"]}/subscriptions/{config["pubSubSubscription"]}",
+    )
+    parser.add_argument(
+        "--window_interval_sec",
+        default=60,
+        type=int,
+        help="Window interval in seconds for grouping incoming messages.",
+    )
+    args, beam_args = parser.parse_known_args()
+
+    run(
+        input_subscription=args.input_subscription,
+        output_table=args.output_table,
+        window_interval_sec=args.window_interval_sec,
+        beam_args=beam_args,
+    )
